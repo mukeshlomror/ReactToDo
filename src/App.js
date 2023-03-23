fffa79d128f7dbf552ab7afc89f659d325f521ec
MukeshLomror <mukeshlomror15@gmail.com>
2023-03-23 17:59:02 +0530

    simple todo app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TodoWrapper from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello mukesh</h1>
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
